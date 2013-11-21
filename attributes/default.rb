45ed330351b312002eeebe326b5eae2018b6274e
Maxim Kremenev <ezo@kremenev.com>
2013-11-21 09:52:45 +0700

    init commit

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+default['kyotocabinet']['version'] = '1.2.76'
+default['kyotocabinet']['url'] = 'http://fallabs.com/kyotocabinet/pkg/kyotocabinet-%s.tar.gz'
+default['kyotocabinet']['checksum'] = 'a4ec70d08ca6c8f510dbc329d5c27b55030d3521'
+default['kyotocabinet']['user'] = 'root'
+default['kyotocabinet']['shell_d'] = '/etc/profile.d'
+
+default['kyotocabinet']['src']['prefix'] = '/usr/local'
+default['kyotocabinet']['src']['options'] = []
+default['kyotocabinet']['src']['tmpdir'] = '/tmp'
+
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'kyotocabinet'
+maintainer       'Go Travel Ltd.'
+maintainer_email 'maxim.kremenev@aviasales.ru'
+license          'All rights reserved'
+description      'Installs/Configures kyotocabinet'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'build-essential'
